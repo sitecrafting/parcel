a2ba8d709538a37656f9c6a1d4dbdac37c54843a
Will Binns-Smith <wbinnssmith@atlassian.com>
2019-05-09 14:18:45 -0700

    Add types for posthtml

--- packages/transformers/html/src/HTMLTransformer.js
+++ packages/transformers/html/src/HTMLTransformer.js
@@ -1,27 +1,36 @@
 // @flow
 
 import {Transformer} from '@parcel/plugin';
+
 import parse from 'posthtml-parser';
-import semver from 'semver';
+import render from 'posthtml-render';
+import nullthrows from 'nullthrows';
 import collectDependencies from './dependencies';
 import extractInlineAssets from './inline';
 
 export default new Transformer({
   canReuseAST(ast) {
-    return ast.type === 'posthtml' && semver.satisfies(ast.version, '^0.4.0');
+    return ast.type === 'posthtml' && ast.version === '0.0.0';
   },
 
   async parse(asset) {
-    let ast = parse(await asset.getCode());
     return {
       type: 'posthtml',
-      version: '0.4.0',
-      program: ast
+      version: '0.0.0',
+      program: parse(await asset.getCode(), {
+        lowerCaseAttributeNames: true
+      })
     };
   },
 
   async transform(asset) {
     collectDependencies(asset);
     return [asset, ...extractInlineAssets(asset)];
+  },
+
+  generate(asset) {
+    return {
+      code: render(nullthrows(asset.ast).program)
+    };
   }
 });
